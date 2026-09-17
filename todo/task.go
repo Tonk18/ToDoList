7a80b5c56c0ff69372803551fe3a6f9cc790ab38
Tonk <charkovskya@gmail.com>
2026-09-17 23:14:44 +0300

    ToDoListFIrstVersion

---
+++ todo/task.go
@@ -0,0 +1,28 @@
+package todo
+
+import "time"
+
+type Task struct {
+	Title       string
+	Discription string
+	IsDone      bool
+
+	CreatedAt time.Time
+	DoneAt    *time.Time
+}
+
+func NewTask(title string, discription string) Task {
+	return Task{
+		Title:       title,
+		Discription: discription,
+		IsDone:      false,
+		CreatedAt:   time.Now(),
+		DoneAt:      nil,
+	}
+}
+
+func (t *Task) Done() {
+	doneTime := time.Now()
+	t.IsDone = true
+	t.DoneAt = &doneTime
+}
